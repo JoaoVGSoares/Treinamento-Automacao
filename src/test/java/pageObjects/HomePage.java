9289f1088a30fc4d91120370849771efae95b3cc
João Soares <joao.v.g.soares@gmail.com>
2021-10-13 21:01:00 -0300

    Atualização do código com teste de pesquisa por produto.

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,7 +1,6 @@
 package pageObjects;
 
 import elementMapper.HomePageElementMapper;
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import utils.Browser;
 
@@ -13,6 +12,22 @@ public class HomePage extends HomePageElementMapper {
 
     public void clickBtnLogin(){
         login.click();
-
+    }
+    public void clickSearch_query_top(){
+        search_query_top.click();
+    }
+    public void sendKeysSearch_query_top(String keys){
+        search_query_top.sendKeys(keys);
+    }
+    public void clickSubmit_search(){
+        submit_search.click();
+    }
+    public void doSearch(String quest){
+        clickSearch_query_top();
+        System.out.println("Clicou na pesquisa");
+        sendKeysSearch_query_top(quest);
+        System.out.println("Digitou o termo");
+        clickSubmit_search();
+        System.out.println("Clicou em pesquisar");
     }
 }
